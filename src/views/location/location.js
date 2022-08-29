503d9a39754290c952043868e93fb5af371371f0
Alexis <alexismorin.pro@gmail.com>
2022-08-29 12:45:16 +0200

    Stars Component

--- src/views/location/location.js
+++ src/views/location/location.js
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import Slideshow from "../../components/slideshow/slideshow";
 import Footer from "../../components/footer/footer";
 import Navbar from "../../components/navbar/navbar";
+import Stars from "../../components/stars/stars";
+import Tag from "../../components/tag/tag"
 
 import './location.scss';
 
@@ -10,7 +12,7 @@ export default function Location() {
 
     const [location, setLocation] = useState({tags:[], equipments:[], pictures:[], rating:'', host:{'name':'', 'picture':''}});
 
-    let { id } = useParams();
+    let { id } = "c67ab8a7"; //useParams();
 
     useEffect(() => {
         fetch('logements.json',
@@ -38,7 +40,22 @@ export default function Location() {
     return (
         <div className="container">
             <Navbar />
-                <Slideshow img={location.pictures}/>
+            <Slideshow img={location.pictures}/>
+            <div className="presentation">
+                <div className="location_header">
+                    <div>
+                        <h2 className="title_locationpage">{location.title}</h2>
+                        <p className="subtitle_locationpage">{location.location}</p>
+                    </div>
+
+                    <div className="main-container-tag">
+                        {location.tags.map((tag) => <Tag content={tag} key={tag}/>)}
+                    </div>
+                </div>
+                <div className="stars-container">
+                    <Stars rating={3} key={1}/>
+                </div>
+            </div>
             <Footer />
         </div>
     )
